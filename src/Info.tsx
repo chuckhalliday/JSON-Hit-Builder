534318f918a3ac0def611f7550a557e739ea7654
chuckhalliday <charleshallidayclarkii@gmail.com>
2023-08-05 00:17:17 -0700

    Update Info.tsx

--- src/Info.tsx
+++ src/Info.tsx
@@ -6,10 +6,10 @@ export default function Info() {
     <div className={styles.infoContainer}>
       {/* Add the information you want to display */}
       <h1>Welcome to my song scaffolding app!</h1>
-      <p>A 3-4 minute composition has been randomly generated in the Redux store </p>
-      <p>All parts are derived off of each other by a custom (evolving) algorithm</p>
-      <p>The buttons above allow you to edit the details to your liking</p>
-      <p>Additional features and bug fixes are being rolled out regularly, stay tuned!</p>
+      <p>A 3-4 minute long composition has been generated in the Redux store </p>
+      <p>All parts are derived off of each other according to a custom algorithm</p>
+      <p>The buttons above allow you to edit the details of each part to your liking</p>
+      <p>Additional features are being rolled out regularly, stay tuned!</p>
     </div>
   );
 };
